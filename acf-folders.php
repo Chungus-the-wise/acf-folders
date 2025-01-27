454347c23d1cae65d53a96f60f45ac0a650fe460
Chungus-the-wise <a.dsrtn@gmail.com>
2025-01-27 23:24:09 +0100

    added taxonomy for acf field group

--- acf-folders.php
+++ acf-folders.php
@@ -1,7 +1,32 @@
 <?php
 
 /**
- * Plugin name: ACF Folders
+ * Plugin Name: ACF Folders
+ * Description:       Add Folders to acf features
+ * Version:           1.0
+ * Requires at least: 5.6
+ * Requires PHP:      7.3
+ * Author:            Antoine Dessertenne
+ * Author URI:        https://github.com/Chungus-the-wise
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       acf-folders
+ * Domain Path:       /languages
+ * Requires Plugins:  advanced-custom-fields
  */
 
-// Silence is golden I guess
+function register_acf_folder_taxonomy() {
+  register_taxonomy(
+	  "acf-folder",
+ 	  "acf-field-group",	  
+          array(
+     		"labels" => array(
+			"name" => "ACF Folders",
+			"singular_name" => "ACF Folder",
+		),
+		"show_in_rest" => true,
+          )
+  );
+}
+
+add_action( "admin_init", "register_acf_folder_taxonomy");
