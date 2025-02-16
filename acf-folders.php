aece0a02e633fb225b87fcac9fcccdc4bc361423
Chungus-the-wise <a.dsrtn@gmail.com>
2025-02-16 16:46:01 +0100

    fixed a few admin issues

--- acf-folders.php
+++ acf-folders.php
@@ -15,18 +15,61 @@
  * Requires Plugins:  advanced-custom-fields
  */
 
+// Adds Folders taxonomy
 function register_acf_folder_taxonomy() {
   register_taxonomy(
-	  "acf-folder",
- 	  "acf-field-group",	  
-          array(
-     		"labels" => array(
-			"name" => "ACF Folders",
-			"singular_name" => "ACF Folder",
-		),
-		"show_in_rest" => true,
-          )
+    "acf-folder",
+    "acf-field-group",	  
+    array(
+      "labels" => array(
+        "name" => "ACF Folders",
+        "singular_name" => "ACF Folder",
+      ),
+      "show_in_menu" => true,
+      "show_in_rest" => true,
+      "public" => true,
+      "show_admin_column" => true,
+      "hierarchical" => true,
+    )
   );
 }
 
-add_action( "admin_init", "register_acf_folder_taxonomy");
+// Adds menu item to ACF parent menu
+function add_acf_folder_menu_item() {
+  add_submenu_page(
+    'edit.php?post_type=acf-field-group',
+    'ACF Folders',
+    'ACF Folders',
+    'edit_posts',
+    'edit-tags.php?taxonomy=acf-folder',
+    '',
+
+  );
+}
+
+// For some reason, we need to alter the taxonomy view query on the post list page
+function folders_list_filter( $query ) {
+  if( ! is_admin() ) {
+   return;
+  }
+  if (
+      $query->query['post_type'] == 'post'
+      //&& isset($query->query['acf-folders'])
+  ) {
+    $folder = $query->query['acf-folder'];
+    $query->set( "post_type", "acf-field-group" );
+    $tax_query = array(
+      array(
+        "taxonomy" => "acf-folder",
+        "field" => "slug",
+        "terms" => array($folder),
+        "operator" => "IN"
+      )
+    );
+    $query->set( "tax_query", $tax_query );
+  }
+
+}
+add_filter( 'pre_get_posts', 'folders_list_filter' );
+add_action( "acf/init", "register_acf_folder_taxonomy");
+add_action( "admin_menu", "add_acf_folder_menu_item" );
